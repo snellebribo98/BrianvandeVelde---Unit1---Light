f63dc0e8f3af61c2193badd8155796742455e957
Brian van de Velde <brianvandevelde@mvdv.home>
2018-11-29 16:45:22 +0100

    Statusbar update

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var button: UIButton!
+    
     var lightOn: Bool = true
     
+    // if the button is pressed the light should change
     @IBAction func buttonPressed(_ sender: AnyObject)
     {
         lightOn = !lightOn
@@ -20,7 +23,23 @@ class ViewController: UIViewController
     
     func updateUI()
     {
-            view.backgroundColor = lightOn ? .white : .black
+        // if button is pressed change background color and color statusbar
+        view.backgroundColor = lightOn ? .white : .black
+        UIApplication.shared.statusBarStyle = lightOn ? .default : .lightContent
+        
+        // if light is white, make text black
+        if lightOn
+        {
+            button.setTitleColor(UIColor.black, for: UIControl.State.normal)
+            button.setTitleColor(UIColor.gray , for: UIControl.State.highlighted)
+        }
+            
+        // if light is black, make text white
+        else if !lightOn
+        {
+            button.setTitleColor(UIColor.white, for: UIControl.State.normal)
+            button.setTitleColor(UIColor.gray, for: UIControl.State.highlighted)
+        }
     }
     
     override func viewDidLoad()
